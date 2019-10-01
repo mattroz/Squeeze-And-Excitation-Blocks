7a6d77ce5c03d59b02ea4062b61bc55ec83b0f15
mattroz <matveyrozanov@gmail.com>
2019-10-01 13:34:29 +0300

    Implelemnt spatial SE block

--- blocks.py
+++ blocks.py
@@ -22,3 +22,18 @@ class cSE(nn.Module):
         x = torch.mul(input_tensor, x)
 
         return x
+
+class sSE(nn.Module):
+
+    def __init__(self, in_channels):
+        super().__init__()
+
+        self.pointwise = nn.Conv2d(in_channels=in_channels, out_channels=1, kernel_size=1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, input_tensor):
+        x = self.pointwise(input_tensor)
+        x = self.sigmoid(x)
+        x = torch.mul(input_tensor, x)
+
+        return x
